2ae1d782239b9b50a421ecf48c63730e84c6084a
MY PC <MY PC@HOME-UEFI1TB>
2018-09-26 18:41:56 +0700

    Project GandyCSitumorang 10116228 PBO6

---
+++ src/kambingstatic.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6.pkg10116228.latihan6.kambingstatic;
+
+/**
+ *
+ * @author Rizki Adam Kurniawan
+ */
+public class kambingstatic {
+    //Nama_Kambing sebagai konstanta
+    public static final String Nama_Kambing = "Mbek";
+    
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(Nama_Kambing  + " memiliki kambing sebanyak " +
+                Mamalia.jumlahKambing);
+    }
+ 
+}
---
+++ src/mamalia.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6.pkg10116228.latihan6.kambingstatic;
+
+/**
+ *
+ * @author Rizki Adam Kurniawan
+ */
+public class Mamalia {
+    //variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+}
